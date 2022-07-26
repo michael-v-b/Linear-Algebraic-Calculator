a76e9a4c7bcf3267e87d534db7c3a08383f90d36
Michael Burton <70928683+Churg@users.noreply.github.com>
2022-07-26 12:23:28 -0400

    Add files via upload

---
+++ tests/Fraction.java
@@ -0,0 +1,154 @@
+
+public class Fraction {
+    public Rint up;
+    public Rint down;
+
+    Rint gcf(Rint a, Rint b) {
+        if (!a.radical && !b.radical) {
+            if (b.value == 0) {
+                return new Rint(a.radical, Math.abs(a.coeff), Math.abs(a.value));
+            } else {
+                return gcf(b, new Rint(false, a.value % b.value));
+            }
+        } else if (a.radical && b.radical) {
+            int tempCoeff = gcf(new Rint(false, a.coeff), new Rint(false, b.coeff)).value;
+            int tempValue = gcf(new Rint(false, a.value), new Rint(false, b.value)).value;
+            return new Rint(true, tempCoeff, tempValue);
+        }
+        Rint temp;
+        if (a.radical) {
+            temp = new Rint(false, a.coeff);
+            return gcf(temp, b);
+        } else {
+            temp = new Rint(false, b.coeff);
+            return gcf(temp, a);
+        }
+    }
+
+    public Fraction(Fraction f) {
+        up = f.up;
+        down = f.down;
+    }
+
+    public Fraction(Rint i, Rint j) {
+        up = i;
+        down = j;
+    }
+
+    public Fraction(Fraction i, Rint j) {
+        up = i.up;
+        down = j.multip(i.down);
+    }
+
+    public Fraction(Rint i, Fraction j) {
+        up = i.multip(j.down);
+        down = i.multip(j.up);
+    }
+
+    public Fraction(Fraction i, Fraction j) {
+        up = i.up.multip(j.down);
+        down = i.down.multip(j.up);
+    }
+
+    public double getFract() {
+        return (double) up.value / down.value;
+    }
+
+    public void simplify() {
+        if (up.value == 0 || up.coeff == 0) {
+            down.coeff = 0;
+            down.value = 0;
+            up.check();
+            down.check();
+            return;
+        }
+        Rint temp = gcf(up, down);
+
+        if (up.value < 0 && down.value < 0) {
+            up.value *= -1;
+            down.value *= -1;
+        }
+
+        if (temp.radical) {
+            up.coeff /= temp.coeff;
+            down.coeff /= temp.coeff;
+            up.check();
+            down.check();
+            return;
+        }
+        // niether are radical
+        if (!up.radical && !down.radical) {
+            up.value /= temp.value;
+            down.value /= temp.value;
+            return;
+            // down is radical
+        } else if (up.radical) {
+            up.coeff /= temp.value;
+            down.value /= temp.value;
+            return;
+            // up is radical
+        } else if (down.radical) {
+            up.value /= temp.value;
+            down.coeff /= temp.value;
+            return;
+            // both are radical
+        } else {
+            up.coeff /= temp.value;
+            down.coeff /= temp.value;
+            return;
+        }
+
+    }
+
+    public Fraction multip(Fraction i) {
+        Fraction output = new Fraction(this);
+        output.up = up.multip(i.up);
+        output.down = down.multip(i.down);
+        return output;
+    }
+
+    public Fraction multip(Rint i) {
+        Fraction output = new Fraction(this);
+        output.up = up.multip(i);
+        return output;
+    }
+
+    public Fraction divide(Fraction i) {
+        Fraction output = new Fraction(this);
+        output.up = up.multip(i.down);
+        output.down = down.multip(i.up);
+        output.simplify();
+        return output;
+    }
+
+    public Fraction divide(Rint i) {
+        Fraction output = new Fraction(this);
+        output.up = up.multip(i);
+        output.simplify();
+        return output;
+    }
+
+    public Fraction sub(Fraction i) {
+        Fraction output = new Fraction(this);
+        output.up = up.multip(i.down);
+        output.up = output.up.sub(i.up.multip(down));
+        output.down = i.down;
+        return output;
+    }
+
+    public String toString() {
+        switch (down.value) {
+            case 1:
+                return "" + up;
+            case -1:
+                if (up.value != 0) {
+                    return "-" + up;
+                } else {
+                    return "" + up;
+                }
+            default:
+                return "" + up + "/" + down;
+        }
+    }
+
+}
---
+++ tests/Main.java
@@ -0,0 +1,251 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+
+    // Rint stands for Radical Integer, allows for exact form
+
+    // used for UI
+    // INPUT METHODS
+    // ==========================================================================================
+    static ArrayList<vector> inputMatrix(Scanner sf) {
+        System.out.println("input dimensions(only use integers): ");
+        ArrayList<vector> matrix;
+        int vectorSize;
+        int vectorNumber;
+        System.out.println("row: ");
+        vectorSize = sf.nextInt(); // row
+        System.out.println("column: ");
+        vectorNumber = sf.nextInt(); // column
+
+        matrix = new ArrayList<vector>(vectorNumber);
+        // instantiates matrix
+        for (int i = 0; i < vectorNumber; i++) {
+            matrix.add(new vector(new ArrayList<>(vectorSize)));
+        }
+        System.out.println("input matrix:");
+        // assigns matrix
+        for (int i = 0; i < vectorSize; i++) {
+            System.out.println("input row [" + i + "]:");
+            for (int j = 0; j < vectorNumber; j++) {
+                Rint temp = new Rint(false, sf.nextInt());
+                matrix.get(j).add(temp);
+            }
+        }
+        return matrix;
+
+    }
+
+    // asks whether output should be in vector form
+    static boolean askVector(Scanner sf) {
+        String vectorMode = "";
+        boolean output;
+        System.out.println("In Vector Form?: y/n?");
+        while (!vectorMode.equals("y") || !vectorMode.equals("n") ||
+                !vectorMode.equals("yes") || !vectorMode.equals("no") ||
+                !vectorMode.equals("Y") || !vectorMode.equals("N")) {
+
+            vectorMode = sf.nextLine();
+            System.out.println("ouput found");
+        }
+        if (vectorMode.equals("y") || vectorMode.equals("Y") || vectorMode.equals("yes")) {
+            output = true;
+        } else {
+            output = false;
+        }
+
+        return output;
+    }
+
+    // inputMatrix with dimensions
+    static ArrayList<vector> inputMatrix(int x, int y, Scanner sf) {
+        System.out.println("input dimensions(only use integers): ");
+        ArrayList<vector> matrix;
+        int vectorSize;
+        int vectorNumber;
+        vectorSize = x;// row
+        vectorNumber = y; // column
+
+        matrix = new ArrayList<vector>(vectorNumber);
+        // instantiates matrix
+        for (int i = 0; i < vectorNumber; i++) {
+            matrix.add(new vector(new ArrayList<>(vectorSize)));
+        }
+        System.out.println("input matrix:");
+        // assigns matrix
+        for (int i = 0; i < vectorSize; i++) {
+            System.out.println("input row [" + i + "]:");
+            for (int j = 0; j < vectorNumber; j++) {
+                Rint temp = new Rint(false, sf.nextInt());
+                matrix.get(j).add(temp);
+            }
+        }
+        return matrix;
+
+    }
+
+    // input Vector
+    static vector inputVector(Scanner sf) {
+        System.out.println("Vector Size:");
+        vector output = new vector();
+        int size = sf.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            Rint temp = new Rint(false, sf.nextInt());
+            output.add(temp);
+        }
+        return output;
+
+    }
+
+    // OUTPUT
+    // METHODS=============================================================================================
+    // an if statement that decides whether to print a method in vector form or not
+    static void matrixDecision(boolean v, ArrayList<vector> matrix) {
+        if (v) {
+            printMatrixVectorMode(matrix);
+        } else {
+            printMatrix(matrix);
+        }
+    }
+
+    // print matrix in output
+    static void printMatrix(ArrayList<vector> matrix) {
+        System.out.println("Matrix: ");
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix.get(i).getSize(); j++) {
+                System.out.print("[" + matrix.get(j).getIndex(i) + "]");
+            }
+            System.out.println("");
+        }
+    }
+
+    // print matrix in vector format
+    static void printMatrixVectorMode(ArrayList<vector> matrix) {
+        System.out.println("Matrix: ");
+        // go through matrix as normal
+        for (int i = 0; i < matrix.get(0).getSize(); i++) {
+            for (int j = 0; j < matrix.get(i).getSize(); j++) {
+
+                // if not in the middle add spaces to compensate for coeff
+                // otherwise write coeff
+                if (i != matrix.get(0).getSize() / 2) {
+                    String tempCoeffString = matrix.get(i).coeff.toString();
+                    for (int k = 0; k < tempCoeffString.length() + 3; k++) {
+                        System.out.print(" ");
+                    }
+                } else {
+                    System.out.print("" + matrix.get(i).coeff.toString() + " * ");
+                }
+                System.out.println("[" + matrix.get(j).getIndex(i) + "] ");
+            }
+        }
+    }
+
+    // printMatrix for Fraction
+    static void printMatrixF(ArrayList<ArrayList<Fraction>> matrix) {
+        System.out.println("Matrix: ");
+        for (int i = 0; i < matrix.get(0).size(); i++) {
+            System.out.print("[");
+            for (int j = 0; j < matrix.size(); j++) {
+                System.out.print(matrix.get(j).get(i).toString() + ", ");
+            }
+            System.out.println("]");
+        }
+    }
+
+    // main method
+    public static void main(String args[]) {
+        Scanner sf;
+        int tempX;
+        int tempY;
+        sf = new Scanner(System.in);
+
+        ArrayList<vector> m1;
+        ArrayList<vector> m2;
+        ArrayList<vector> matrixOut;
+        String operations[] = { "help", "orthogonal", "orthonormal" };
+        System.out.println("type your operation");
+        boolean inputLoop = true;
+        boolean vectorMode;
+        while (inputLoop) {
+            switch (sf.nextLine()) {
+                case "help":
+                    for (String s : operations) {
+                        System.out.println(s);
+                    }
+                    break;
+                case "matrixSum":
+                    System.out.println("input 2 matrices");
+                    System.out.println("input dimensions(only use integers): ");
+                    System.out.println("row: ");
+                    tempX = sf.nextInt(); // row
+                    System.out.println("column: ");
+                    tempY = sf.nextInt(); // column
+
+                    m1 = inputMatrix(tempX, tempY, sf);
+                    m2 = inputMatrix(tempX, tempY, sf);
+                    matrixOut = Matrix.add(m1, m2);
+                    vectorMode = askVector(sf);
+                    System.out.println("Sum:");
+                    matrixDecision(vectorMode, matrixOut);
+                    break;
+
+                case "matrixProduct":
+                    System.out.println("input 2 matrices");
+                    System.out.println("input dimensions(only use integers): ");
+                    System.out.println("row: ");
+                    tempX = sf.nextInt(); // row
+                    System.out.println("column: ");
+                    tempY = sf.nextInt(); // column
+                    System.out.println("input 2 matrices");
+
+                    m1 = inputMatrix(tempX, tempY, sf);
+                    m2 = inputMatrix(tempY, tempX, sf);
+                    matrixOut = Matrix.multip(m1, m2);
+
+                    vectorMode = askVector(sf);
+                    System.out.println("Product:");
+                    matrixDecision(vectorMode, matrixOut);
+
+                    break;
+                case "orthogonal":
+                    m1 = inputMatrix(sf);
+                    printMatrix(m1);
+                    matrixOut = orthogonal.orthogonalize(m1);
+                    vectorMode = askVector(sf);
+                    System.out.println("Orthogonal :");
+                    matrixDecision(vectorMode, matrixOut);
+                    break;
+                case "orthonormal":
+                    m1 = inputMatrix(sf);
+                    printMatrix(m1);
+                    matrixOut = orthogonal.orthonormalize(m1);
+                    vectorMode = askVector(sf);
+                    System.out.println("Orthonormal Set:");
+                    matrixDecision(vectorMode, matrixOut);
+                    break;
+                case "dotProduct":
+                    vector v1 = inputVector(sf);
+                    vector v2 = inputVector(sf);
+                    Fraction out = orthogonal.dotProduct(v1, v2);
+                    System.out.println("Result: " + out + "");
+
+                    break;
+                case "RREF":
+                    m1 = inputMatrix(sf);
+                    ArrayList<ArrayList<Fraction>> RREF = Matrix.RREF(m1);
+                    printMatrixF(RREF);
+                    break;
+                case "diagonlize":
+                    break;
+                case "quit":
+                    inputLoop = false;
+                    break;
+            }
+        }
+
+    }
+
+}
